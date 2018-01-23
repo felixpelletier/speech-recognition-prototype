cb416b3383001ad34dddc129fff0be6e6085426c
Félix Pelletier <felixpelletier23@gmail.com>
2018-01-23 18:18:28 -0500

    Started a c generator

--- generate_c_data.m
+++ generate_c_data.m
@@ -1,6 +1,15 @@
 function generate_c_data( trained_matrices, output_filename )
 
+header_name = strcat(output_filename, '.h');
+source_name = strcat(output_filename, '.c');
+
 fileId = fopen(output_filename, 'w');
+
+for i = 1:length(trained_matrices)
+    
+end
+
+
 fclose(fileId);
 
 end

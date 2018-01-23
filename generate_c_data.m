88c2f1e0d80f852959d2a47640e410e01a69b033
Félix Pelletier <felixpelletier23@gmail.com>
2018-01-23 18:11:07 -0500

    Started a c generator

---
+++ generate_c_data.m
@@ -0,0 +1,7 @@
+function generate_c_data( trained_matrices, output_filename )
+
+fileId = fopen(output_filename, 'w');
+fclose(fileId);
+
+end
+
---
+++ main_generate_c_data.m
@@ -0,0 +1,10 @@
+FILE_COUNT = 250;
+FILE_COUNT_FOR_TRAINING = 50;
+
+one = get_trained_matrix('google_dataset/one', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
+two = get_trained_matrix('google_dataset/two', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
+three = get_trained_matrix('google_dataset/three', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
+        
+trained_matrices = {one, two, three};
+
+generate_c_data(trained_matrices, 'c_version/training_data.h');

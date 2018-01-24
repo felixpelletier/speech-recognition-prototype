f095856b152bd6888469b0cb3cff84fac962930d
Félix Pelletier <felixpelletier23@gmail.com>
2018-01-23 23:46:39 -0500

    C generator

--- generate_c_data.m
+++ generate_c_data.m
@@ -1,16 +1,82 @@
-function generate_c_data( trained_matrices, output_filename )
+function generate_c_data( words, trained_matrices, output_path )
 
-header_name = strcat(output_filename, '.h');
-source_name = strcat(output_filename, '.c');
+assert(length(words) == length(trained_matrices));
 
-fileId = fopen(output_filename, 'w');
+header_path = strcat(output_path, '.h');
+source_path = strcat(output_path, '.c');
+[~, filename, ~] = fileparts(output_path);
 
-for i = 1:length(trained_matrices)
-    
+guard_name = strcat('_DESIGNIV__', upper(filename), '__H_'); 
+struct_name = 'word_training_gaussians';
+substruct_name = 'word_training_gaussian';
+
+NumVariables = trained_matrices{1}.NumVariables;
+NumComponents = trained_matrices{1}.NumComponents;
+
+headerId = fopen(header_path, 'w');
+
+fprintf(headerId, '#ifndef %s \n#define %s\n\n', guard_name, guard_name);
+
+fprintf(headerId, '#define TRAINING_DATA_MFCC_COUNT %i\n', NumVariables);
+fprintf(headerId, '#define TRAINING_DATA_GAUSSIAN_COUNT %i\n', NumComponents);
+
+fprintf(headerId, '\n');
+
+for i = 1:length(words)
+    fprintf(headerId, '#define TRAINING_DATA_%s %i\n', upper(words{i}), i-1);
 end
 
+fprintf(headerId, '\n');
 
-fclose(fileId);
+fprintf(headerId, 'typedef struct %s {\n', substruct_name);
+fprintf(headerId, '    float proportion;\n');
+fprintf(headerId, '    float mu[TRAINING_DATA_MFCC_COUNT];\n');
+fprintf(headerId, '    float sigma[TRAINING_DATA_MFCC_COUNT];\n');
+fprintf(headerId, '} %s_t;\n', substruct_name);
+
+fprintf(headerId, '\n');
+
+fprintf(headerId, 'typedef struct %s {\n', struct_name);
+fprintf(headerId, '    %s_t gaussians[TRAINING_DATA_GAUSSIAN_COUNT];\n', substruct_name);
+fprintf(headerId, '} %s_t;\n', struct_name);
+
+fprintf(headerId, '\n');
+
+fprintf(headerId, 'const %s_t TRAINING_DATA[] = {\n', struct_name);
+
+for i = 1:length(words)
+    trained_matrix = trained_matrices{i};
+    fprintf(headerId, '    {\n');
+    fprintf(headerId, '        {\n');
+    for j = 1:trained_matrix.NumComponents
+        fprintf(headerId, '            {\n');
+        fprintf(headerId, '                %.6gf,\n', trained_matrix.ComponentProportion(j));
+        fprintf(headerId, '                {');
+        for k = 1:trained_matrix.NumVariables
+            fprintf(headerId, '%.6gf', trained_matrix.mu(j, k));
+            if (k < trained_matrix.NumVariables)
+                fprintf(headerId, ', ');
+            end
+        end
+        fprintf(headerId, '},\n');
+        fprintf(headerId, '                {');
+        for k = 1:trained_matrix.NumVariables
+            fprintf(headerId, '%.6gf', trained_matrix.Sigma(1, k, j));
+            if (k < trained_matrix.NumVariables)
+                fprintf(headerId, ', ');
+            end
+        end
+        fprintf(headerId, '}\n');
+        fprintf(headerId, '            },\n');
+    end
+    fprintf(headerId, '        },\n');
+    fprintf(headerId, '    },\n');
+end
+
+fprintf(headerId, '};\n');
+
+fprintf(headerId, '#endif //%s\n', guard_name);
+fclose(headerId);
 
 end
 
--- main_generate_c_data.m
+++ main_generate_c_data.m
@@ -1,10 +1,11 @@
-FILE_COUNT = 250;
+ vFILE_COUNT = 250;
 FILE_COUNT_FOR_TRAINING = 50;
 
-one = get_trained_matrix('google_dataset/one', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
-two = get_trained_matrix('google_dataset/two', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
-three = get_trained_matrix('google_dataset/three', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
+%one = get_trained_matrix('google_dataset/one', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
+%two = get_trained_matrix('google_dataset/two', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
+%three = get_trained_matrix('google_dataset/three', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MaxNumberOfFiles', FILE_COUNT_FOR_TRAINING);
         
+word = {'one', 'two', 'three'};
 trained_matrices = {one, two, three};
 
-generate_c_data(trained_matrices, 'c_version/training_data.h');
+generate_c_data(word, trained_matrices, 'c_version/training_data');

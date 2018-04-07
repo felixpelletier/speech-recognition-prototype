7a287aea39b57f746df76aee26353ed7b5c83677
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-04-07 11:45:08 -0400

    Changed clic for cliquer

--- main_generate_c_data.m
+++ main_generate_c_data.m
@@ -4,12 +4,12 @@ MinimumAmplitude = -35;
 MaxNumberOfFiles = 100;
 
 accueil = get_trained_matrix('new_dataset/accueil', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MinimumAmplitude', MinimumAmplitude, 'MaxNumberOfFiles', MaxNumberOfFiles);
-clic = get_trained_matrix('new_dataset/clic', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MinimumAmplitude', MinimumAmplitude, 'MaxNumberOfFiles', MaxNumberOfFiles);
+cliquer = get_trained_matrix('new_dataset/cliquer', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MinimumAmplitude', MinimumAmplitude, 'MaxNumberOfFiles', MaxNumberOfFiles);
 lacher = get_trained_matrix('new_dataset/lacher', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MinimumAmplitude', MinimumAmplitude, 'MaxNumberOfFiles', MaxNumberOfFiles);
 retour = get_trained_matrix('new_dataset/retour', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MinimumAmplitude', MinimumAmplitude, 'MaxNumberOfFiles', MaxNumberOfFiles);
 tenir = get_trained_matrix('new_dataset/tenir', 'NumberMFCCKept', NumberMFCCKept, 'NumberMFCCCalculated', NumberMFCCCalculated, 'MinimumAmplitude', MinimumAmplitude, 'MaxNumberOfFiles', MaxNumberOfFiles);
         
-words = {'accueil', 'clic', 'lacher', 'retour', 'tenir'};
-trained_matrices = {accueil, clic, lacher, retour, tenir};
+words = {'accueil', 'cliquer', 'lacher', 'retour', 'tenir'};
+trained_matrices = {accueil, cliquer, lacher, retour, tenir};
 
 generate_c_data(words, trained_matrices, 'c_version/training_data');
